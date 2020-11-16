832e3a5767a8c82cda9c24e9072b5117bf3e4d82
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-11-16 16:18:44 +0100

    esercizio con buns

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data
+Altro possibile Bonus: gestire una funzione con un evento nel modo Vue.*/
+
+//istanziamo vue agganciando l'istanza del nostro div container
+var container = new Vue ({
+    el : "#container",
+    data : {
+        titolo : "Benvenuto nel mondo Vue!",
+        immagine : "./img/vue-symbol.jpg"
+    }
+});
+
